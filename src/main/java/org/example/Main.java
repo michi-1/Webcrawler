cba2fb4d0e0db69812d5375bb807852663c2ffb9
NahidNouraniVereecke <wi21b089@fh-technikum-wien.at>
2023-11-22 10:23:48 +0100

    sendEmail Funktion not completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,17 @@
 package org.example;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,7 +27,7 @@ public class Main {
             String searchUrl = buildSearchUrl(url, parameters);
 
 
-            Document document = Jsoup.connect("https://www.autoscout24.at/lst/mercedes-benz").get();
+            Document document = Jsoup.connect("https://www.autoscout24.at/lst/mercedes-benz/108").get();
 
 
             String title = document.title();
@@ -30,10 +35,12 @@ public class Main {
 
 
             Elements productElements = document.select(".list-page-item");
+            String emailContent = "Title: " + title + "\nProducts:\n";
             for (Element productElement : productElements) {
-
+                emailContent += "Product: " + productElement.text() + "\n";
                 System.out.println("Product: " + productElement.text());
             }
+            sendEmail("nahid_noorani@yahoo.com", "Car Information", emailContent);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -51,5 +58,43 @@ public class Main {
             stringBuilder.deleteCharAt(stringBuilder.length() - 1);
         }
         return stringBuilder.toString();
+    }
+    private static void sendEmail(String to, String subject, String content) {
+
+        String from = "nourani.nahid@gmail.com";
+        String password = "";
+
+
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+
+        try {
+
+            MimeMessage message = new MimeMessage(session);
+
+            message.setFrom(new InternetAddress(from));
+
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            message.setSubject(subject);
+
+            message.setText(content);
+
+            Transport.send(message);
+            System.out.println("Sent email successfully!");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
     }
 }
