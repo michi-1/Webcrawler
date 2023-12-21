b0684cf92a498f35b5687a87c8b8ce84af3b45c7
NahidNouraniVereecke <wi21b089@fh-technikum-wien.at>
2023-12-21 13:23:27 +0100

    Email Text

---
+++ src/main/java/org/example/EmailMessageBuilder.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class EmailMessageBuilder {
+
+    public static String buildEmailMessage() {
+        Date now = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        String formattedDate = dateFormat.format(now);
+
+        // E-Mail-Text erstellen
+        StringBuilder message = new StringBuilder();
+        message.append("Sehr geehrter Kunde,<br><br>");
+        message.append("Wir freuen uns, Ihnen unseren neuesten Fahrzeugbericht präsentieren zu können. ");
+        message.append("In diesem Bericht finden Sie eine Übersicht über die neuesten Fahrzeuge auf dem Markt, ");
+        message.append("die Ihren spezifischen Kriterien entsprechen.<br><br>");
+        message.append("Unsere Experten haben sorgfältig die Daten von verschiedenen Quellen gesammelt und analysiert, ");
+        message.append("um Ihnen eine Auswahl an hochwertigen Fahrzeugen vorzustellen. ");
+        message.append("Wir verstehen, wie wichtig es ist, das perfekte Auto zu finden, ");
+        message.append("das Ihren Bedürfnissen und Wünschen entspricht.<br><br>");
+        message.append("In diesem Bericht werden Sie Informationen zu den neuesten Automodellen, Marken, Preisen und Jahresmodellen finden. ");
+        message.append("Wir haben sicherzustellen versucht, dass alle aufgeführten Fahrzeuge Ihren Anforderungen entsprechen und eine Vielzahl von Optionen bieten.<br><br>");
+        message.append("Es war uns eine Freude, diesen Bericht für Sie zu erstellen, ");
+        message.append("und wir hoffen, dass er Ihnen bei Ihrer Suche nach einem neuen Fahrzeug hilfreich ist. ");
+        message.append("Bei Fragen oder weiterem Bedarf stehen wir Ihnen gerne zur Verfügung.<br><br>");
+        message.append("Vielen Dank, dass Sie unsere Dienstleistungen in Anspruch nehmen. ");
+        message.append("Wir wünschen Ihnen viel Erfolg bei der Suche nach Ihrem Traumauto!<br><br>");
+        message.append("Mit freundlichen Grüßen,<br><br>");
+        message.append("Ihr Carcrawlpro Team<br>");
+        message.append("Datum und Uhrzeit: ").append(formattedDate);
+        message.append("<br><br>");
+
+
+        return message.toString();
+    }
+}
--- src/main/java/org/example/webcrawlerui/WebCrawlerUI.java
+++ src/main/java/org/example/webcrawlerui/WebCrawlerUI.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.example.EmailMessageBuilder;
 import org.example.EmailSender;
 import org.example.WebCrawler;
 
@@ -100,23 +101,7 @@ public class WebCrawlerUI extends Application {
         String password = "idvg yxfh gept zkpa";
         String mailTo = "nourani.nahid@gmail.com";
         String subject = "Begrüßung zu Ihrem individuellen Fahrzeugbericht";
-        String message = "Sehr geehrter Kunde,<br><br>"
-                + "Wir freuen uns, Ihnen unseren neuesten Fahrzeugbericht präsentieren zu können. "
-                + "In diesem Bericht finden Sie eine Übersicht über die neuesten Fahrzeuge auf dem Markt, "
-                + "die Ihren spezifischen Kriterien entsprechen.<br><br>"
-                + "Unsere Experten haben sorgfältig die Daten von verschiedenen Quellen gesammelt und analysiert, "
-                + "um Ihnen eine Auswahl an hochwertigen Fahrzeugen vorzustellen. "
-                + "Wir verstehen, wie wichtig es ist, das perfekte Auto zu finden, "
-                + "das Ihren Bedürfnissen und Wünschen entspricht.<br><br>"
-                + "In diesem Bericht werden Sie Informationen zu den neuesten Automodellen, Marken, Preisen und Jahresmodellen finden. "
-                + "Wir haben sicherzustellen versucht, dass alle aufgeführten Fahrzeuge Ihren Anforderungen entsprechen und eine Vielzahl von Optionen bieten.<br><br>"
-                + "Es war uns eine Freude, diesen Bericht für Sie zu erstellen, "
-                + "und wir hoffen, dass er Ihnen bei Ihrer Suche nach einem neuen Fahrzeug hilfreich ist. "
-                + "Bei Fragen oder weiterem Bedarf stehen wir Ihnen gerne zur Verfügung.<br><br>"
-                + "Vielen Dank, dass Sie unsere Dienstleistungen in Anspruch nehmen. "
-                + "Wir wünschen Ihnen viel Erfolg bei der Suche nach Ihrem Traumauto!<br><br>"
-                + "Mit freundlichen Grüßen,<br><br>"
-                + "Ihr Carcrawlpro Team<br><br>";
+        String message = EmailMessageBuilder.buildEmailMessage();
 
 
 
